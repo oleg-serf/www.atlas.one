dea747ea9f6d7a8dde762356bcd243806d888f9a
oleg-serf <olegserfcompany@gmail.com>
2020-11-10 15:55:47 -0500

    minor issue

--- src/pages/terms/index.jsx
+++ src/pages/terms/index.jsx
@@ -1,7 +1,14 @@
-// import React from "react"
+import React, { useEffect } from "react"
 
 const Terms = () => {
-    window.open("https://www.atlas.one/terms", "_self")
+  useEffect(()=> {
+    if (typeof window != "undefined")
+      window.open("https://www.atlas.one/terms", "_self")
+  })
+
+  return(
+    <></>
+  );
 }
 
 export default Terms
