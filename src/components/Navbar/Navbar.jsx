8c822ae93ee7ba71523616859627a1fee453b173
oleg-serf <olegserfcompany@gmail.com>
2021-01-04 16:40:15 -0500

    revert logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { AnchorLink } from "gatsby-plugin-anchor-links"
 import { GiHamburgerMenu } from "react-icons/gi"
 import { AiOutlineClose } from "react-icons/ai"
 import { VscMegaphone } from "react-icons/vsc"
-import SnowAnimation from "../SnowAnimation"
+// import SnowAnimation from "../SnowAnimation"
 import { useWindowSize, getSrcSet } from "../../hooks/getwidth"
 import "./navbar.scss"
 
@@ -133,23 +133,13 @@ export default function Header({ isTransparentHeader = false }) {
         <Link
           className="flex items-center flex-shrink-0 text-white pl-3 lg:pl-0 cursor-pointer w-auto xl:w-1/5"
           to="/"
-        >
-          { isOffset ?  
-            <img
-              className="logo"
-              srcSet={ getSrcSet(currentLogo, logoSize.width) }
-              alt="Altas Logo"
-              ref={logoRef}
-            /> : 
-            <SnowAnimation>
-              <img
-                className="logo"
-                srcSet={ getSrcSet(currentLogo, logoSize.width) }
-                alt="Altas Logo"
-                ref={logoRef}
-              />
-            </SnowAnimation>
-          }
+        > 
+          <img
+            className="logo"
+            srcSet={ getSrcSet(currentLogo, logoSize.width) }
+            alt="Altas Logo"
+            ref={logoRef}
+          />
         </Link>
         <div className="block lg:hidden">
           {!isExpanded ? (
